0d93545d76c3a70cf2aaf14deebf80d0c40ef171
Angga-XII <prianka.anggara@gmail.com>
2025-05-30 06:02:53 +0700

    feat: added lightning css, framer motion, and enabled spa mode in config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,21 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+export default defineConfig(
+  {
+    css: {
+      transformer: 'lightningcss',
+      lightningcss: {
+        targets: browserslistToTargets(browserslist('>= 0.25%')),
+        cssModules: true
+      }
+    },
+    build: {
+      cssMinify: 'lightningcss'
+    },
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  }
+);
