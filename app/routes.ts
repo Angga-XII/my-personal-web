e88f96232ef24ff01ed568461016ffaf7be35280
Angga-XII <prianka.anggara@gmail.com>
2025-06-01 13:52:00 +0700

    added null page to handle chrome dev tools sending request to the listed page

--- app/routes.ts
+++ app/routes.ts
@@ -2,8 +2,10 @@ import { type RouteConfig, index, route,  } from "@react-router/dev/routes";
 
 export default [
     // index("routes/home.tsx"),
+    // route("profile", "routes/profile.tsx"),
     index("routes/profile.tsx"),
     route("resume", "routes/resume.tsx"),
     route("message", "routes/messages.tsx"),
     route("portfolio", "routes/portofolio.tsx"),
+    route("/.well-known/appspecific/com.chrome.devtools.json","routes/nullDebug.tsx")
 ] satisfies RouteConfig;
